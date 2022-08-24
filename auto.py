7a59c95c1193ba1274cbfb16e891a7391b731073
John Doe <49613009+Algortex@users.noreply.github.com>
2022-08-25 07:56:16 +0800

    修复startStationCode问题

--- auto.py
+++ auto.py
@@ -195,7 +195,7 @@ while True:
     writeLog("[时间] 目前时间为" + now.strftime(TIMEFORMAT))
     if (not FINISHEDCAPTCHA) and (((weekday == 1) and (now.hour == 19 and (now.minute >= 50 and now.minute <= 59))) and CAPTCHA == 2):
         FINISHEDCAPTCHA = True
-        referrerURL = f"https://i.hzmbus.com/webhtml/ticket_details?xlmc_1={BUS_STOPS[startStationCode]}&xlmc_2={BUS_STOPS[endStationCode]}&xllb=1&xldm={ROUTE}&code_1={startStationCode}&code_2={endStationCode}"
+        referrerURL = f"https://i.hzmbus.com/webhtml/ticket_details?xlmc_1={BUS_STOPS[START]}&xlmc_2={BUS_STOPS[END]}&xllb=1&xldm={ROUTE}&code_1={START}&code_2={END}"
         referrerURL = parse.quote_plus(referrerURL)
         my_cap = crack_ali.slide(hzmbus, headers, referrerURL, "FFFF0N0000000000A95D", "nc_other_h5", "6748c822ee91e", TRACK)
         if my_cap == None:
