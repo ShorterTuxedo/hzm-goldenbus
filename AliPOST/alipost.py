e36ac86b99f0dff21b26d2c0cc8380fcea49b56e
John Doe <49613009+Algortex@users.noreply.github.com>
2022-09-02 22:39:08 +0800

    Add files via upload

---
+++ AliPOST/alipost.py
@@ -0,0 +1,112 @@
+from flask import Flask, request
+from selenium.webdriver.common.action_chains import ActionChains
+import random
+import time
+import os
+from selenium import webdriver
+from urllib import parse
+stealthminjs = None
+with open('stealth.min.js', 'r') as f:
+    stealthminjs = f.read()
+app = Flask(__name__)
+
+GUIJIHTML = '''<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <script src="https://g.alicdn.com/AWSC/AWSC/awsc.js"></script>
+</head>
+<body>
+<div id="nc"></div>
+<script>
+    // 实例化nc
+    AWSC.use("nc", function (state, module) {
+        // 初始化
+        window.nc = module.init({
+            // 应用类型标识。它和使用场景标识（scene字段）一起决定了滑动验证的业务场景与后端对应使用的策略模型。您可以在阿里云验证码控制台的配置管理页签找到对应的appkey字段值，请务必正确填写。
+            appkey: "MYAPPID",
+            //使用场景标识。它和应用类型标识（appkey字段）一起决定了滑动验证的业务场景与后端对应使用的策略模型。您可以在阿里云验证码控制台的配置管理页签找到对应的scene值，请务必正确填写。
+            scene: "MYSCENE",
+            // 声明滑动验证需要渲染的目标ID。
+            renderTo: "nc",
+            //前端滑动验证通过时会触发该回调参数。您可以在该回调参数中将会话ID（sessionId）、签名串（sig）、请求唯一标识（token）字段记录下来，随业务请求一同发送至您的服务端调用验签。
+            success: function (data) {
+                document.body.innerHTML += "<br /><br /><h2>打码成功！</h2><p><textarea id=\\"mync\\">" + JSON.stringify(data) + "</textarea></p>";
+            },
+            // 滑动验证失败时触发该回调参数。
+            fail: function (failCode) {
+                document.body.innerHTML += "<br /><br /><h2>打码失败！</h2><p><textarea id=\\"mync\\">" + JSON.stringify(failCode) + "</textarea></p>";
+            },
+            // 验证码加载出现异常时触发该回调参数。
+            error: function (errorCode) {
+                document.body.innerHTML += "<br /><br /><h2>验证码出现异常！</h2><p><textarea id=\\"mync\\">" + JSON.stringify(errorCode) + "</textarea></p>";
+            }
+        });
+    })
+</script>
+</body>
+</html>'''
+
+@app.route('/captcha', methods=['POST'])
+def captcha():
+    global GUIJIHTML
+    s1 = time.time()
+    appid = request.json["appId"]
+    scene = request.json["scene"]
+    MYHTMLFILE = GUIJIHTML.replace("MYAPPID", appid).replace("MYSCENE", scene)
+    with open("guiji.html", "w", encoding="UTF-8") as myh:
+        myh.write(MYHTMLFILE)
+    s2 = time.time()
+    print("Stage 1 done.")
+    browser = None
+    try:
+        from selenium.webdriver.firefox.options import Options
+        options = Options()
+        options.headless = True
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("dom.webdriver.enabled", False)
+        profile.set_preference('useAutomationExtension', False)
+        profile.update_preferences()
+        desired = DesiredCapabilities.FIREFOX
+        browser = webdriver.Firefox(options=options,firefox_profile=profile, desired_capabilities=desired)
+    except Exception:
+        myRandomChromeUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+        print("Your useragent is", myRandomChromeUA, ".")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument("user-agent="+myRandomChromeUA)
+        options.add_argument("--headless")
+        browser = webdriver.Chrome(options=options)
+        # 调用函数在页面加载前执行脚本
+        browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {'source': stealthminjs})
+    browser.get(os.path.abspath("guiji.html"))
+    slideBtn = browser.find_element_by_css_selector(".btn_slide")
+    slideOffsetWidth = browser.execute_script("return (document.querySelector(\".nc_scale\").clientWidth - document.querySelector(\".nc_iconfont\").clientWidth);")
+    actions = ActionChains(browser)
+    offsets = [] # 规避可疑轨迹
+    while slideOffsetWidth > 0:
+        myOffset = random.randint(50, 120)
+        if slideOffsetWidth < myOffset:
+            myOffset = slideOffsetWidth
+        slideOffsetWidth -= myOffset
+        offsets.append(myOffset)
+    print("Track:", offsets)
+    actions.click_and_hold(slideBtn).perform()
+    for slideWidth in offsets:
+        actions.move_by_offset(xoffset=slideWidth,yoffset=0).perform()
+    actions.release().perform()
+    print("Captcha done.")
+    myData = None
+    while myData == None:
+        try:
+            myData = browser.find_element_by_css_selector("#mync").get_attribute("innerHTML")
+        except Exception:
+            myData = None
+    s3 = time.time()
+    print(f"Stage 2 done.\nStage 1 took {s2-s1} seconds.")
+    print(f"Stage 2 took {s3-s2} seconds.")
+    return myData
+
+
+app.run(host="0.0.0.0", port=8001)
--- crack_ali.py
+++ crack_ali.py
@@ -3,63 +3,17 @@ import json
 import time
 import random
 from urllib import parse
-
+import requests
 # TRACK = ""
 
 def slide(session, headers, referrerURL, APPID, scene, ncSessionID, TRACK):
-    # global TRACK
-    headers2 = headers
-    headers2["Referer"] = referrerURL
-    headers2["accept"] = "*/*"
-    headers2.pop("content-type", None)
-    captcha = session.get("https://ynuf.aliapp.org/w/wu.json", headers=headers2)
-    UMIDTOKEN = captcha.text
-    # print(UMIDTOKEN)
-    UMIDTOKEN = captcha.text.split("umx.wu('")[1].split("');}catch")[0]
-    # print("UMID TOKEN 是",UMIDTOKEN)
-    # print(len(UMIDTOKEN))
-    C_TOKEN = APPID + ":" + scene + ":" + str(int(time.time())) + ":" + str(random.random())
-    init_payload = {
-        "a": APPID,
-        "t": C_TOKEN,
-        "scene": scene,
-        "lang": "cn",
-        "v": "v1.2.20",
-        "href": referrerURL,
-        "comm": "{}",
-        "callback": "initializeJsonp_" + "".join(str(random.random()).split("."))
-    }
-    INIT_URL = "https://cf.aliyun.com/nocaptcha/initialize.jsonp?" + parse.urlencode(init_payload)
-    captcha = session.get(INIT_URL, headers=headers2)
-    # print(captcha.text)
-    analyze_payload = {
-        "a": APPID,
-        "t": C_TOKEN,
-        "n": TRACK,
-        "p": {
-            "umidToken": UMIDTOKEN,
-            "ncSessionID": ncSessionID
-        },
-        "scene": scene,
-        "asyn": 0,
-        "lang": "cn",
-        "v": 1,
-        "callback": "jsonp_" + "".join(str(random.random()).split("."))
-    }
-    ANALYZE_URL = "https://cf.aliyun.com/nocaptcha/analyze.jsonp?" + parse.urlencode(analyze_payload)
-    captcha = session.get(ANALYZE_URL, headers=headers2)
-    # print(captcha.text)
-    myResult = json.loads(captcha.text.split("(")[1].split(")")[0])
-    if myResult["result"]["code"] == 300:
+    try:
+        mySlider = requests.post("http://localhost:8001/captcha", json={
+            "appId": APPID,
+            "scene": scene
+        }).json()
+        print("验证码破解成功")
+        return mySlider
+    except Exception:
         print("验证码破解失败")
         return None
-    else:
-        print("验证码破解成功")
-    # if myResult[]
-    captcha_params = {
-        "sessionId": myResult["result"]["csessionid"],
-        "sig": myResult["result"]["value"],
-        "token": C_TOKEN
-    }
-    print(captcha_params)
-    return captcha_params
