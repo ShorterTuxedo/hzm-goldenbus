aedcd237c7abbf671dea334aec1e187e246136c3
John Doe <49613009+Algortex@users.noreply.github.com>
2022-09-03 08:11:03 +0800

    Update alipost.py

--- AliPOST/alipost.py
+++ AliPOST/alipost.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 import random
 import time
 import os
+from pyvirtualdisplay import Display
 from selenium import webdriver
 from urllib import parse
 stealthminjs = None
@@ -47,6 +48,25 @@ GUIJIHTML = '''<!DOCTYPE html>
 </body>
 </html>'''
 
+try:
+    if os.name == "posix":
+        display = Display(visible=0, size=(1024, 768))
+        display.start()
+    myRandomChromeUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+    print("Your useragent is", myRandomChromeUA, ".")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("user-agent="+myRandomChromeUA)
+    options.add_argument("--headless")
+    if os.name == "posix":
+        options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(options=options)
+    # 调用函数在页面加载前执行脚本
+    browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {'source': stealthminjs})
+except Exception as e:
+    print("失败")
+
 @app.route('/captcha', methods=['POST'])
 def captcha():
     global GUIJIHTML
@@ -58,22 +78,7 @@ def captcha():
         myh.write(MYHTMLFILE)
     s2 = time.time()
     print("Stage 1 done.")
-    browser = None
-    try:
-        myRandomChromeUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
-        print("Your useragent is", myRandomChromeUA, ".")
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
-        options.add_argument("user-agent="+myRandomChromeUA)
-        options.add_argument("--headless")
-        browser = webdriver.Chrome(options=options)
-        # 调用函数在页面加载前执行脚本
-        browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {'source': stealthminjs})
-    except Exception as e:
-        print("失败")
-        return '{"sessionId":"","sig":"","token":""}'
-    browser.get(os.path.abspath("guiji.html"))
+    browser.get("file://"+os.path.abspath("guiji.html"))
     slideBtn = browser.find_element_by_css_selector(".btn_slide")
     slideOffsetWidth = browser.execute_script("return (document.querySelector(\".nc_scale\").clientWidth - document.querySelector(\".nc_iconfont\").clientWidth);")
     actions = ActionChains(browser)
