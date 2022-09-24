7daa2eb942b569ecc0b71e29f3a646bb712fadf0
Algortex <algortex@algortex.com>
2022-09-24 19:54:39 +0800

    嵌入hzmbus_hash库

--- auto.py
+++ auto.py
@@ -5,7 +5,7 @@ import datetime
 import ddddocr
 import json
 import smtplib
-import crack_ali
+import hzmbus_hash;import crack_ali
 import acw_sc_v2
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -18,7 +18,7 @@ from urllib import parse
 
 CAPTCHA = 2 # 2 = 阿里云， 1 = 文字
 
-info = json.loads(open("info.json", "r").read())
+info = json.loads(open("info.json", "r").read());myURL = "https://i.hzmbus.com/webhtml/index.html"
 
 DATE = "1970-01-01" # 替代日期
 
@@ -203,7 +203,7 @@ eightPM = 20
 DF = "%Y-%m-%d"
 FINISHEDCAPTCHA = False
 writeLog("[提示] 等待中...")
-my_cap = {"sessionId": "", "sig": "", "token": ""}
+my_cap = {"sessionId": "", "sig": "", "token": ""};myhash = hzmbus_hash.HZMHash()
 while True:
     #timeArray=time.localtime(time.time()+dt)
     #jsTime=time.strftime("%Y-%m-%d %H:%M:%S")
@@ -215,7 +215,7 @@ while True:
     writeLog("[时间] 目前时间为" + now.strftime(TIMEFORMAT))
     if (weekday != 1) or (hour >= eightPM):
         if CAPTCHA == 1:
-            my_cap = {"sessionId": "", "sig": "", "token": ""}
+            my_cap = {"sessionId": "", "sig": "", "token": ""};myhash = hzmbus_hash.HZMHash()
         while True:
             gotTicket = False
             while not gotTicket:
@@ -402,11 +402,11 @@ while True:
                         FINISHEDCAPTCHA = True
                         referrerURL = f"https://i.hzmbus.com/webhtml/ticket_details?xlmc_1={BUS_STOPS[START]}&xlmc_2={BUS_STOPS[END]}&xllb=1&xldm={ROUTE}&code_1={START}&code_2={END}"
                         referrerURL = parse.quote_plus(referrerURL)
-                        my_cap = crack_ali.slide(hzmbus, headers, referrerURL, "FFFF0N0000000000A95D", "nc_other_h5", "6748c822ee91e", TRACK)
+                        myhash.activate_browser(url=myURL);my_cap = crack_ali.slide(hzmbus, headers, referrerURL, "FFFF0N0000000000A95D", "nc_other_h5", "6748c822ee91e", TRACK)
                         if my_cap == None:
                             break
 
-                homepage = hzmbus.post("https://i.hzmbus.com/webh5api/ticket/buy.ticket", headers=headers, json={
+                homepage = hzmbus.post("https://i.hzmbus.com/webh5api/ticket/buy.ticket", headers=headers, json=myhash.set_token_web({
                 "ticketData": DATE,
                 "lineCode": ROUTE,
                 "startStationCode": START,
@@ -434,7 +434,7 @@ while True:
                 "joinType": "WEB",
                 "version": "2.7.202207.1213",
                 "equipment": "PC"
-                });print(str(homepage.content, encoding="UTF-8"))#;time.sleep(3)
+                }));print(str(homepage.content, encoding="UTF-8"))#;time.sleep(3)
 
                 writeLog("[购票结果] 购票结果为 " + str(homepage.content, encoding="UTF-8"))
 
